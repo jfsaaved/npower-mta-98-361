1e5f2673fb99ccebaa69c630b668357281907b6a
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2019-08-07 10:32:52 -0400

    first init

---
+++ MTA-1/MTA-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MTA_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            int x = 16;
+            int y = 5;
+
+            Console.WriteLine(x / y);
+
+            Console.WriteLine(15 % 6);
+
+        }
+    }
+}
