8fe3241accbcb295680a6ebb7d0652a75edd75db
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2019-08-07 11:26:42 -0400

    First concatenation in C#

--- mta-1.1-variables/mta-1.1-variables/Form1.cs
+++ mta-1.1-variables/mta-1.1-variables/Form1.cs
@@ -20,11 +20,19 @@ namespace mta_1._1_variables
         private void Button1_Click(object sender, EventArgs e)
         {
             string firstName;
+            string messageText;
+
+            messageText = "Your Name Is: ";
+
+            firstName = textBox1.Text;
+            MessageBox.Show(messageText + firstName);
+
+            /*
             firstName = textBox1.Text;
             MessageBox.Show(firstName);
 
             firstName = "Home and Learn";
-            MessageBox.Show(firstName);
+            MessageBox.Show(firstName);*/
         }
 
         private void TextBox1_TextChanged(object sender, EventArgs e)
