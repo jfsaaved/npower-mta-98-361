f610cafab6da7a8841f81dde6632649aa61b57cd
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2019-08-07 11:46:10 -0400

    simple arithmetic

---
+++ mta-1.1-variables-arithmetic/mta-1.1-variables-arithmetic/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mta_1._1_variables_arithmetic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            int firstNumber;
+            int secondNumber;
+            int integerAnswer;
+
+            firstNumber = 10;
+            secondNumber = 32;
+
+            integerAnswer = firstNumber + secondNumber;
+
+            MessageBox.Show(integerAnswer.ToString());
+        }
+    }
+}
