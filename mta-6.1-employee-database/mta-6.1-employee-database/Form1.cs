1ad798e5950ac4df3b2a31e13f453e8bc768bf63
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2019-08-16 11:21:38 -0400

    Databases

---
+++ mta-6.1-employee-database/mta-6.1-employee-database/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mta_6._1_employee_database {
+    public partial class Form1 : Form {
+
+        DatabaseConnection objConnection;
+        string conString;
+
+        DataSet ds;
+        DataRow dRow;
+
+        int maxRows;
+        int inc = 0;
+
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            try {
+                objConnection = new DatabaseConnection();
+                conString = Properties.Settings.Default.EmployeeConnectionString;
+
+                objConnection.connection_string = conString;
+                objConnection.Sql = Properties.Settings.Default.SQL;
+
+                ds = objConnection.GetConnection;
+                maxRows = ds.Tables[0].Rows.Count;
+
+                NavigateRecords();
+
+            } catch (Exception err) {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+        private void NavigateRecords () {
+            dRow = ds.Tables[0].Rows[0];
+            textBox1.Text = dRow.ItemArray.GetValue(1).ToString();
+            textBox2.Text = dRow.ItemArray.GetValue(2).ToString();
+            textBox3.Text = dRow.ItemArray.GetValue(3).ToString();
+            textBox4.Text = dRow.ItemArray.GetValue(4).ToString();
+        }
+    }
+}
---
+++ mta-6.1-employee-database/mta-6.1-employee-database/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("mta-6.1-employee-database")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("mta-6.1-employee-database")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5afbc8ab-6f57-4778-b758-926b46a42506")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
